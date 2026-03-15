ad94ae8234fc7de3918fe63b8a2cc46d613c0b13
noco <nicolas.reig@icloud.com>
2026-03-15 12:01:10 +0000

    started implementation of TV Shows support

---
+++ series/index.php
@@ -0,0 +1,45 @@
+<?php
+	include '../config.php';
+
+	// Check if a series is specified
+	if (isset($_GET['movie'])) {
+		$series = htmlspecialchars(urldecode(basename($_GET['movie'])), ENT_QUOTES, 'UTF-8');
+	} else {
+		header('Location: /');
+		exit();
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+	<title><?php echo str_replace('_', ':', $series); ?></title>
+	<link rel="stylesheet" href="/main.css">
+	<link rel="stylesheet" href="/index.css">
+	<link rel="icon" href="/favicon.png" type="image/png">
+</head>
+<body>
+	<header>
+		<?php echo "<h1>" . str_replace('_', ':', $series) . "</h1>"; ?>
+	</header>
+	<main>
+		<div class="movies">
+		<?php
+			foreach (glob(MOVIES_PATH . "$series/*", GLOB_ONLYDIR) as $season) {
+				echo "<h2>" . str_replace('_', ':', basename($season)) . "</h2>";
+				$episodes = glob(MOVIES_PATH . "$series/$season/*", GLOB_ONLYDIR);
+				foreach ($episodes as $episode) {
+					$episode = basename($episode);
+					$URLepisode = urlencode($episode);
+					$title = str_replace('_', ':', $episode);
+					// TODO
+					echo "<a href='player/?movie=$URLepisode' class='movie'><img src='/serve.php?movie=$URLepisode&file=thumbnail.jpg'><h3>$title</h3></a>";
+				}
+			}
+		?>
+		</div>
+	</main>
+</body>
+</html>
